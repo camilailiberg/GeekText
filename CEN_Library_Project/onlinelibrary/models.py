5cffed966c4375198bbd34d10c9f56f814b478f5
Ernesto Hernandez <pochy305@yahoo.es>
2021-02-20 14:58:38 -0500

    Wishlist Functionality
    Ability to create wishlist and host books inside the wishlist.

--- CEN_Library_Project/onlinelibrary/models.py
+++ CEN_Library_Project/onlinelibrary/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class WishList(models.Model):
+    name = models.CharField(max_length=200)
+    primary = models.BooleanField(True, False)
+    max = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    wishlist = models.ForeignKey(WishList, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    added = models.BooleanField()
+
+    def __str__(self):
+        return self.name
--- CEN_Library_Project/onlinelibrary/urls.py
+++ CEN_Library_Project/onlinelibrary/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("wishlist/<int:id>", views.index, name="index"),
     path("wishlist", views.wishlist, name="wishlist"),
 ]
