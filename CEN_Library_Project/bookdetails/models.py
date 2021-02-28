54d54c84989b707b820910ad275646dae7e97310
cnovelle <gonzalezchristianlopez@gmail.com>
2021-02-28 16:59:02 -0500

    Made seperate app and added images

---
+++ CEN_Library_Project/bookdetails/models.py
@@ -0,0 +1,35 @@
+import uuid
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.CharField(max_length=4000, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
+
