7f89d04e1c445b50373b400e5a9e8d020b634463
cnovelle <gonzalezchristianlopez@gmail.com>
2021-02-28 16:49:33 -0500

    Made seperate app and added images

--- CEN_Library_Project/CEN_Library_Project/settings.py
+++ CEN_Library_Project/CEN_Library_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'onlinelibrary.apps.OnlinelibraryConfig',
+    'bookdetails.apps.BookdetailsConfig',
     'rest_framework',
 ]
 
