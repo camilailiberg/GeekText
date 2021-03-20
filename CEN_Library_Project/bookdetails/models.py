84408d6d450a7aad0929c6fa97246ebc561dd4e4
EGonzalez2771 <77640379+EGonzalez2771@users.noreply.github.com>
2021-03-20 15:29:30 -0400

    Sprint #4 Rating Review System

--- CEN_Library_Project/bookdetails/models.py
+++ CEN_Library_Project/bookdetails/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 class Author(models.Model):
     author = models.CharField(max_length=200)
     bio = models.CharField(max_length=4000)
 
-    def __str__(self):
+    def str(self):
         return self.author
 
 
@@ -16,13 +17,6 @@ class Book(models.Model):
         Author,
         related_name='booker',
     )
-
-    def validate_rating(value):
-
-        if value <= 5 and value >= 0:
-            return value
-        else:
-            raise ValidationError("The rating is out of the bounds of 0 - 5")
 
     GENRES = (
         ('Sci-fi', 'Sci-fi'),
@@ -35,15 +29,41 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='bookImages', default='bookImages/No_image.jpg')
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=200, default='other', choices=GENRES)
-    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0, validators=[validate_rating])
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     description = models.CharField(max_length=4000)
     publisher = models.CharField(max_length=200, default='Unknown')
     release_date = models.DateField(default='Unknown')
+    bestseller = models.BooleanField(default=False)
 
-    def __str__(self):
+    def str(self):
         return self.title
 
+
+# Rating and Review Portion
+class RatingReview(models.Model):
+    RATE = (
+        ('1', 1),
+        ('2', 2),
+        ('3', 3),
+        ('4', 4),
+        ('5', 5),
+    )
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, default=0, related_name="ratings")
+    rating = models.CharField(choices=RATE, max_length=128, default=1)
+    review = models.CharField(max_length=128, default='Write Review')
+    username = models.CharField(max_length=128, default="")
+    nickName = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=128, default="")
+    last_name = models.CharField(max_length=128, default="")
+    book_title = models.CharField(max_length=128, default="")
+
+
+    def str(self):
+        return self.rating
+
+    class Meta:
+        managed = True
+        db_table = 'ratingReview_bookdetails'
 
 
 
--- CEN_Library_Project/bookdetails/views.py
+++ CEN_Library_Project/bookdetails/views.py
@@ -1,18 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from .models import RatingReview
+from .forms import RatingForm
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def index(response, book_id):
+def index(request, book_id):
+    # Rating and Review Portion
+    rating = RatingReview.objects.all()
+    username = RatingReview.objects.all()
+    form = RatingForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    # Book Details Portion
     book = Book.objects.get(id=book_id)
     author = book.authors.all()
-    return render(response, "bookdetails/book_detail.html", {'book': book, 'author': author})
+    ratings = book.ratings.all()
+    average_rating = 0
+
+    if len(ratings) != 0:
+
+        for rate in ratings:
+            average_rating += int(rate.rating)
+
+        average_rating /= len(ratings)
+        average_rating = round(average_rating, 1)
+
+    context = {
+        'form': form,
+        'rating': rating,
+        'username': username,
+        'book': book, 'author': author,
+        'ratings': ratings,
+        'average_rating': average_rating
+    }
+    return render(request, "bookdetails/book_detail.html", context)
 
 
 def home(response):
     return HttpResponse("<h1>hi</h1>")
+
+
     
 
 
