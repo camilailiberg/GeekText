05cad9fca3705178c968ecaa2c6c234fe484ffee
camila <camilailiberg@gmail.com>
2021-03-29 02:42:32 -0400

    in onlinelibrary urls.py file I made the default view to be Browse view instead of the home.html file

--- CEN_Library_Project/onlinelibrary/urls.py
+++ CEN_Library_Project/onlinelibrary/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views
+from browse import views
 
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.post_list, name="post_list"),
 ]
